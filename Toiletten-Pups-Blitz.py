6269fca0e69deb12793d5885e05e17e88216d859
dieLaura <planologin@posteo.de>
2021-12-04 19:28:12 +0100

    we turned a desktop-only-app into a programm that works with the lights in our bathroom.

--- Toiletten-Pups-Blitz.py
+++ Toiletten-Pups-Blitz.py
@@ -3,9 +3,69 @@
 
 import random
 import sys
+import time
+import utime
+
+from machine import Pin
+from neopixel import NeoPixel
+
+# CLASSES # Function for corners, that need to be ignored, because they are not existing in real life (e.g. Corners of a room)
+
+class point:
+    def __init__(self, x:int, y:int):
+        self.x = x
+        self.y = y
+    def drawthethingfunction(self, feld, thing):
+        feld[self.y][self.x] = thing
+    def getthething(self, feld):
+        if self.x > spaltenanzahl-1 or self.y > zeilenanzahl-1 or (self.x < 0 or self.y < 0):
+            return "#"
+        return feld[self.y][self.x]
+
+class box: 
+    def __init__(self, upperleftcorner: point, lowerrightcorner: point):
+        self.upperleftcorner = upperleftcorner
+        self.lowerrightcorner = lowerrightcorner
+    def drawtheboxfunction(self, feld, thing):
+        for y in range(self.upperleftcorner.y, self.lowerrightcorner.y+1):
+            for x in range(self.upperleftcorner.x, self.lowerrightcorner.x+1):
+                p = point(x,y)
+                p.drawthethingfunction(feld, thing)
+
+#Table of constants
+
+wall = "#"
+spaltenanzahl = 126
+zeilenanzahl = 16
+upperleftcorner1 = point(116,9)
+lowerrightcorner1 = point(125,15)
+upperleftcorner2 = point(0,12)
+lowerrightcorner2 = point(20,15)
+box1 = box(upperleftcorner2, lowerrightcorner2)
+box2 = box(upperleftcorner1, lowerrightcorner1)
+pin = Pin(23, Pin.OUT)
+np = NeoPixel(pin, 1863, bpp=3)
+
+
+# Utill functions
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('reality_of_things', 'o@ij7Y}:<-i+}S]=#1..*ew{#,n6(n2$fa1aD&SK')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+def start_webrepl():
+    import webrepl
+    webrepl.start(password='Toilet,.,')
 
 # Creates a  "field" of  the size chosen by the user.
-
+# There is a class and a function for creating "boxes" where there are no "lights" or functions are not allowed to go
+    
 def feldmachen(zeilenanzahl, spaltenanzahl):
     feld = []
     for i in range(0, zeilenanzahl):
@@ -18,44 +78,48 @@ def feldmachen(zeilenanzahl, spaltenanzahl):
 # prints the field.
 
 def felddrucken(feld):
-    for zeile in feld:
-        print(zeile)
-    print()
+    for i, pixel in enumerate(feld_to_led(feld)):
+        np[i] = pixel
+    np.write()
 
 # Chooses random Spawnpoint and turn it "On".
 
-def spawnpointfunction(zeilenanzahl, spaltenanzahl):
-    x = random.randint(0, zeilenanzahl-1)
-    y = random.randint(0, spaltenanzahl-1)
-    spawnpoint = [x,y]
+def spawnpointfunction(feld, zeilenanzahl, spaltenanzahl):
+    print("y1: " + str(utime.ticks_ms()))
+    y = random.randint(0, zeilenanzahl-1)
+    print("y2: " + str(utime.ticks_ms()))
+    x = random.randint(0, spaltenanzahl-1)
+    spawnpoint = point(x,y)
+    if spawnpoint.getthething(feld) == wall:
+        return spawnpointfunction(feld, zeilenanzahl, spaltenanzahl)
     return spawnpoint
 
 
 # Chooses random Goalpoint. That can't be Spawnpoint.
 
-def goalpointfunction(zeilenanzahl, spaltenanzahl, spawnpoint):
+def goalpointfunction(feld, zeilenanzahl, spaltenanzahl, spawnpoint):
     goalpoint = spawnpoint
-    while goalpoint == spawnpoint:
-        goalpoint = spawnpointfunction(zeilenanzahl, spaltenanzahl)
+    while goalpoint == spawnpoint or goalpoint.getthething(feld) == wall:
+        goalpoint = spawnpointfunction(feld, zeilenanzahl, spaltenanzahl)
     return goalpoint
 
 # Turns all of the points it gets on.
 
-def turnonfunction(feld, point):
-    feld[point[0]][point[1]] = "0"
+def turnonfunction(feld, point: point):
+    point.drawthethingfunction(feld, "0")
 
 # This turns "on" the goalpoint with a X. It is like a tresure to find.
 
-def markgoalpoint(feld, point):
-    feld[point[0]][point[1]] = "X"
+def markgoalpoint(feld, point: point):
+    point.drawthethingfunction(feld,"X")
 
 #  If Spawnpoint is next to Goal: EXPLOSION! BOOOOOM! Possibly will blind people. ¯\_(ツ)_/¯
 
-def explosionfunction(feld, goalpoint, spawnpoint):
-    testgoalpoint = goalpoint.copy()
-    testgoalpoint[0] = testgoalpoint[0]-spawnpoint[0]
-    testgoalpoint[1] = testgoalpoint[1]-spawnpoint[1]
-    if abs(testgoalpoint[0])*abs(testgoalpoint[1]) == 1 or abs(testgoalpoint[0])+abs(testgoalpoint[1]) == 1 :
+def explosionfunction(feld, goalpoint: point, spawnpoint: point):
+    testgoalpoint = point(goalpoint.x,goalpoint.y)
+    testgoalpoint.x = testgoalpoint.x-spawnpoint.x
+    testgoalpoint.y = testgoalpoint.y-spawnpoint.y
+    if abs(testgoalpoint.x)*abs(testgoalpoint.y) == 1 or abs(testgoalpoint.x)+abs(testgoalpoint.y) == 1 :
         explosion = []
         for i in range(0, zeilenanzahl):
             expzeile = []
@@ -72,23 +136,13 @@ def explosionfunction(feld, goalpoint, spawnpoint):
 # in preparation of stepping-function, i define a "is the goalpoint next to the current position"-function
 
 def closetogoalfunction(goalpoint, currentpoint):
-    testgoalpoint = goalpoint.copy()
-    testgoalpoint[0] = testgoalpoint[0]-currentpoint[0]
-    testgoalpoint[1] = testgoalpoint[1]-currentpoint[1]
-    if abs(testgoalpoint[0])*abs(testgoalpoint[1]) == 1 or abs(testgoalpoint[0])+abs(testgoalpoint[1]) == 1 :
+    testgoalpoint = point(goalpoint.x,goalpoint.y)
+    testgoalpoint.x = testgoalpoint.x-currentpoint.x
+    testgoalpoint.y = testgoalpoint.y-currentpoint.y
+    if abs(testgoalpoint.x)*abs(testgoalpoint.y) == 1 or abs(testgoalpoint.x)+abs(testgoalpoint.y) == 1 :
         return True
     else:
         return False
-
-
-# The following two functions want to check if the currentpoint cannot continue because it is trapped between "walls" or already entered fields.
-
-def getvaluefunction(feld, coordinate):
-    if coordinate[0] > zeilenanzahl-1 or coordinate[1] > spaltenanzahl-1 or (coordinate[0] < 0 or coordinate[1] < 0):
-        return "0"
-    else:
-        value = feld[coordinate[0]][coordinate[1]]
-        return value
 
 def yougotmecorneredfunction(feld):
     felddrucken(feld)
@@ -97,42 +151,43 @@ def yougotmecorneredfunction(feld):
 
 # All 8 Points surrounding the currentpoint will be checked, wether they are options to go to.
 
-def whereshouldigofromherefunction(feld, currentpoint):
-    possiblepointupperleft = (currentpoint[0]-1, currentpoint[1]-1)
-    possiblepointuppermid = (currentpoint[0]-1, currentpoint[1]-0)
-    possiblepointupperright = (currentpoint[0]-1, currentpoint[1]+1)
-    possiblepointleft = (currentpoint[0]-0, currentpoint[1]-1)
-    possiblepointright = (currentpoint[0]-0, currentpoint[1]+1)
-    possiblepointlowerleft = (currentpoint[0]+1, currentpoint[1]-1)
-    possiblepointlowermid = (currentpoint[0]+1, currentpoint[1]-0)
-    possiblepointlowerright = (currentpoint[0]+1, currentpoint[1]+1)
+def whereshouldigofromherefunction(feld, goalpoint: point, currentpoint: point):
+    possiblepointupperleft = point(currentpoint.x-1, currentpoint.y-1)
+    possiblepointuppermid = point(currentpoint.x-0, currentpoint.y-1)
+    possiblepointupperright = point(currentpoint.x+1, currentpoint.y-1)
+    possiblepointleft = point(currentpoint.x-1, currentpoint.y-0)
+    possiblepointright = point(currentpoint.x+1, currentpoint.y)
+    possiblepointlowerleft = point(currentpoint.x-1, currentpoint.y+1)
+    possiblepointlowermid = point(currentpoint.x, currentpoint.y+1)
+    possiblepointlowerright = point(currentpoint.x+1, currentpoint.y+1)
     initialdistancedictionary = {possiblepointupperleft : 0, possiblepointuppermid : 0, possiblepointupperright : 0, possiblepointleft : 0, possiblepointright : 0, possiblepointlowerleft : 0, possiblepointlowermid : 0, possiblepointlowerright : 0}
     # From here on, the distance to the target point is calculated for each point. This is collected in a dictionary.
     distancevaluedictionary = {}
-    for point in initialdistancedictionary:
-        if getvaluefunction(feld, point) != "0":
-            distancevaluedictionary[point] = pythagorasfunction(howmanyfieldstogofunction(goalpoint, point))    
+    for p in initialdistancedictionary:
+        if p.getthething(feld) == " ":
+            distancevaluedictionary[p] = pythagorasfunction(proximitytogoalfunction(goalpoint, p))    
     if not distancevaluedictionary:
         yougotmecorneredfunction(feld)
-        
+
+    print("pointtogoto 1: " + str(utime.ticks_ms()))    
     pointtogoto = calculateprobabilitiesandchooseanextpositionfunction(distancevaluedictionary)
-    print("Point to go to: (%d, %d)" % (pointtogoto[0], pointtogoto[1]))
+    print("pointtogoto 2: " + str(utime.ticks_ms()))  
+    #print("Point to go to: (%d, %d)" % (pointtogoto.x, pointtogoto.y))
     return pointtogoto
 
-# As a part of whereshouldigofunction it fiends out how many fields on the x-axis and y-axis are between currentpoint and goalpoint.
+# As a part of whereshouldigofunction it finds out how many fields on the x-axis and y-axis are between currentpoint and goalpoint.
 
-def howmanyfieldstogofunction(goalpoint, pointinquestion):
-    y = abs(pointinquestion[1] - goalpoint[1])
-    x = abs(pointinquestion[0] - goalpoint[0])
-    numberoffieldstogotuple = (x , y)
-    return numberoffieldstogotuple
-#    return a, b Tupel and give it to pythagorasfunciton
+def proximitytogoalfunction(goalpoint: point, pointinquestion: point):
+    y = abs(pointinquestion.y - goalpoint.y)
+    x = abs(pointinquestion.x - goalpoint.x)
+    return point(x, y)
+#    return a point and give it to pythagorasfunciton
 
 # pythagoras. You know him... 
 
-def pythagorasfunction(numberoffieldstogotuple):
-    a = numberoffieldstogotuple[0]
-    b = numberoffieldstogotuple[1]
+def pythagorasfunction(numberoffieldstogo: point):
+    a = numberoffieldstogo.x
+    b = numberoffieldstogo.y
     c = (a**2+b**2)**0.5
     return c
     #gib c zu Liste
@@ -160,7 +215,7 @@ def calculateprobabilitiesandchooseanextpositionfunction(distancevaluedictionary
         distancevaluedictionary[point] = abs(distancevaluedictionary[point]) 
     # Take the values of all points to the power of 2 (or any other value), so that the weighting of the point with the smallest distance increases.   
     for point in distancevaluedictionary:
-        distancevaluedictionary[point] = distancevaluedictionary[point]**1.4
+        distancevaluedictionary[point] = distancevaluedictionary[point]**1.2
     # jetzt muss von allen Punkten die Summe der Werte errechnet werden um dann damit für jeden Punkt seinen Prozentwert vom gesamten zu errechnen, um dass das als die "Wahrscheinlichkeit" der Auswahl dieses Punktes zu nutzen.
     sumofalldistances = 0
     for point in distancevaluedictionary:
@@ -180,8 +235,8 @@ def calculateprobabilitiesandchooseanextpositionfunction(distancevaluedictionary
     chosensteptopoint = random.choice(drawpot)
     return chosensteptopoint
 
-def gotothenextpointandmakeitshinefunction(feld, currentpoint):
-    nextpointtogoto = whereshouldigofromherefunction(feld, currentpoint)
+def gotothenextpointandmakeitshinefunction(feld, goalpoint, currentpoint):
+    nextpointtogoto = whereshouldigofromherefunction(feld, goalpoint, currentpoint)
     currentpoint = nextpointtogoto
     turnonfunction(feld,currentpoint)
     felddrucken(feld)
@@ -190,37 +245,58 @@ def gotothenextpointandmakeitshinefunction(feld, currentpoint):
 def makealightningfunction(feld, goalpoint, spawnpoint):
     currentpoint = spawnpoint
     while closetogoalfunction(goalpoint, currentpoint) == False:        
-        input("Press Enter to continue...")
-        feld, currentpoint = gotothenextpointandmakeitshinefunction(feld, currentpoint)
+#        time.sleep(0.02)
+        feld, currentpoint = gotothenextpointandmakeitshinefunction(feld, goalpoint, currentpoint)
     else:
         turnonfunction(feld,goalpoint)
         felddrucken(feld)
         sys.exit(0)
 
+def feld_to_led(feld):
+    fb = []
+    for i, row in enumerate(feld):
+        if i % 2 == 1:
+            row = reversed(row)
+        for cell in row:
+            if cell == wall:
+                continue
+            if cell == " ":
+                fb.append((0,0,0))
+            if cell == "0" or cell == "X":
+                fb.append((255,255,255))
+    return fb
+
 # this is where the magic happens!
 
-print()
-zeilenanzahl = int(input("Wie viele Zeilen soll das Feld haben? "))
-spaltenanzahl = int(input("Wie viele Spalten soll das Feld haben? "))
-print()
-print("Das Feld hat " + str(zeilenanzahl) + " Zeilen und " + str(spaltenanzahl) + " Spalten.")
-print()
-        
-feld = feldmachen(zeilenanzahl, spaltenanzahl)
-felddrucken(feld)
+def main():
+    #do_connect()
+    #start_webrepl()
 
-spawnpoint = spawnpointfunction(zeilenanzahl, spaltenanzahl)
-goalpoint = goalpointfunction(zeilenanzahl, spaltenanzahl, spawnpoint)
+    feld = feldmachen(zeilenanzahl, spaltenanzahl)
 
-print("Der Startpunkt liegt bei " + str(spawnpoint) + ".")
-print("Der Zielpunkt liegt bei " + str(goalpoint) + ".")
+    box1.drawtheboxfunction(feld, wall)
+    box2.drawtheboxfunction(feld, wall)
 
-turnonfunction(feld,spawnpoint)
-markgoalpoint(feld, goalpoint)
-felddrucken(feld)
+    felddrucken(feld)
 
-explosionfunction(feld, goalpoint, spawnpoint)
+    spawnpoint = spawnpointfunction(feld, zeilenanzahl, spaltenanzahl)
+    goalpoint = goalpointfunction(feld, zeilenanzahl, spaltenanzahl, spawnpoint)
 
-# this is practically the main function
+    print("Der Startpunkt liegt bei " + str(spawnpoint.x) + "," + str(spawnpoint.y) + ".")
+    print("Der Zielpunkt liegt bei " + str(goalpoint.x) + "," + str(goalpoint.y)  + ".")
 
-makealightningfunction(feld, goalpoint, spawnpoint)
+    turnonfunction(feld,spawnpoint)
+    markgoalpoint(feld, goalpoint)
+    felddrucken(feld)
+
+    explosionfunction(feld, goalpoint, spawnpoint)
+
+    # this is practically the main function
+
+    makealightningfunction(feld, goalpoint, spawnpoint)
+
+#if __name__ == "__main__":
+while True:
+    main()
+#    time.sleep(random.randint(2,4))
+
